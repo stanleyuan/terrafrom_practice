025ba9ef88efc789306e0beb6eb66642e73fb9da
stanleyuan <stanley830912@gmail.com>
2019-05-14 00:13:45 +0800

    Remove s3 and one ec2 instance

--- example.tf
+++ example.tf
@@ -10,15 +10,3 @@ resource "aws_instance" "example" {
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
 }
-
-resource "aws_s3_bucket" "iii" {
-  bucket = "iii-bucket"
-  acl    = "private"
-}
-
-resource "aws_instance" "depends_on" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-
-  depends_on = ["aws_s3_bucket.iii"]
-}
